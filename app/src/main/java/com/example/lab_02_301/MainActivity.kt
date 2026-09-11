7f1795df663555a9da62e59a0d783114252779db
rishan-98 <srrishan13398@gmail.com>
2026-09-10 19:49:00 -0600

    Updated

--- app/src/main/java/com/example/lab_02_301/MainActivity.kt
+++ app/src/main/java/com/example/lab_02_301/MainActivity.kt
@@ -23,6 +23,9 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.lab_02_301.ui.theme.Lab02301Theme
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.ui.graphics.Color
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -35,6 +38,7 @@ class MainActivity : ComponentActivity() {
                     CityListScreen(
                         cities = cityRepository.cities,
                         onAddCity = { cityRepository.addCity(it) },
+                        onDeleteCity = { cityRepository.removeCity(it) },
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -49,10 +53,8 @@ class CityRepository {
         "Edmonton", "Vancouver", "Moscow",
         "Sydney", "Berlin", "Vienna",
         "Tokyo", "Beijing", "Osaka",
-        "New Delhi"
-    )
+        "New Delhi")
 
-// Get a read-only list for the UI to display
     val cities: List<String>
         get() = _cities
 
@@ -60,15 +62,19 @@ class CityRepository {
         _cities.add(city)
     }
 
-
+    fun removeCity(city: String){
+        _cities.remove(city)
+    }
 }
 @Composable
 fun CityListScreen(
     cities: List<String>,
     onAddCity: (String) -> Unit,
+    onDeleteCity: (String) -> Unit,
     modifier: Modifier = Modifier
 ) {
     var newCityName by remember { mutableStateOf(value = "") }
+    var selectedCity by remember { mutableStateOf<String?>(null) }
 
     Column(modifier = modifier.fillMaxSize()) {
         Row(modifier = modifier.padding(all = 16.dp)) {
@@ -92,22 +98,51 @@ fun CityListScreen(
                 Text("Add City")
             }
         }
+
+        Button(
+            onClick = {
+                selectedCity?.let {
+                    onDeleteCity(it)
+                    selectedCity = null
+                }
+            },
+            enabled = selectedCity != null,
+            modifier = Modifier
+                .padding(horizontal = 16.dp)
+                .fillMaxWidth()
+        ){
+            Text(if (selectedCity != null)
+                "Delete \"$selectedCity\""
+                else "Delete City"
+            )
+        }
+
+
         // LazyColumn is the Compose replacement for a basic scrolling ListView
         LazyColumn(modifier = modifier.fillMaxSize()) {
             items(cities) { city ->
-                CityRow(city = city)
+                CityRow(
+                    city = city,
+                    isSelected = city == selectedCity,
+                    onClick = {
+                        selectedCity = if (selectedCity == city) null
+                        else city
+                    }
+                )
             }
         }
     }
 }
 
 @Composable
-fun CityRow(city: String) {
+fun CityRow(city: String, isSelected : Boolean, onClick: () -> Unit) {
     Text(
         text = city,
         fontSize = 28.sp,
         modifier = Modifier
             .fillMaxWidth()
+            .background(if (isSelected) Color.LightGray else Color.Transparent)
+            .clickable { onClick() }
             .padding(horizontal = 18.dp, vertical = 14.dp)
     )
 }
